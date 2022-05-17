22e3133949431b7d86cf1e0daf29752369edb1c0
Arman <persakiss@gmail.com>
2022-05-17 15:17:56 +0400

    Finished iterators, added review

--- iterators.js
+++ iterators.js
@@ -1,4 +1,5 @@
 //// Iterator Map ////
+/// Map returns a new array
 const animals = ['Hen', 'elephant', 'llama', 'leopard', 
 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
 const secretMessage = animals.map(animal => {
@@ -12,6 +13,7 @@ console.log(secretMessage); // returns "HelloWorld";
 
 
 //// Iterator forEach ////
+/// ForEach cycles through every item in array
 const fruits = ['mango', 'papaya', 'pineapple', 'apple'];
 fruits.forEach(fruit => {
   console.log(`I want to eat a ${fruit}`);
@@ -22,6 +24,7 @@ fruits.forEach(fruit => {
 
 
 //// Iterator Filter ////
+/// Returns array with conditions
 const randomNumbers = [375, 200, 3.14, 7, 13, 852];
 const smallNumbers = randomNumbers.filter(number => {
   return number < 250;
@@ -41,6 +44,7 @@ const longFavoriteWords = favoriteWords.filter(word => {
 
 
 //// IndexOf Iterator ////
+/// IndexOf provides the array index 
 const animals = ['hippo', 'tiger', 'lion', 'seal', 'cheetah', 'monkey', 'salamander', 'elephant'];
 const foundAnimal = animals.findIndex(word => {
   return word === 'elephant';
@@ -50,3 +54,66 @@ const startsWithS = animals.findIndex(word => {
     return word[0] === 's';
   }) // Finds index of word starting with s
 /// End IndexOf Iterator ////
+
+
+
+
+
+//// Reduce Iterator //////
+/// Reduce adds values
+const newNumbers = [1, 3, 5, 7];
+const newSum = newNumbers.reduce((accumulator, currentValue) => {
+  console.log('The value of accumulator: ', accumulator);
+  console.log('The value of currentValue: ', currentValue);
+  return accumulator + currentValue;
+} /* a second value could go here, eg ', 10' */ );
+
+console.log(newSum);
+///// End Reduce Iterator ///
+
+
+
+
+//// Every, Filter, Some Iterators /////
+/// Every, Some returns boolean value
+/// Filter returns array
+const words = ['unique', 'uncanny', 'pique', 'oxymoron', 'guise'];
+
+console.log(words.some((word) => {
+  return word.length < 6;
+
+const interestingWords = words.filter(word => {
+  return word.length > 5;
+})
+
+console.log(interestingWords.every((word) => { 
+  return word.length > 5;
+} ));
+//// Every, Filter, Some Iterators //// 
+
+
+
+///// Iterator Review /////////
+const cities = ['Orlando', 'Dubai', 'Edinburgh', 'Chennai', 'Accra', 'Denver', 'Eskisehir', 'Medellin', 'Yokohama'];
+
+const nums = [1, 50, 75, 200, 350, 525, 1000];
+
+//  Choose a method that will return undefined
+cities.forEach(city => console.log('Have you visited ' + city + '?'));
+
+// Choose a method that will return a new array
+const longCities = cities.filter(city => city.length > 7);
+
+// Choose a method that will return a single value
+const word = cities.reduce((acc, currVal) => {
+  return acc + currVal[0]
+}, "C");
+
+console.log(word)
+
+// Choose a method that will return a new array
+const smallerNums = nums.map(num => num - 5);
+
+// Choose a method that will return a boolean value
+nums.every(num => num < 0);
+//////////////////
