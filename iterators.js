6965a7fdb39d5d5a8854adc993d824b92f2ae4e7
Arman <persakiss@gmail.com>
2022-05-17 14:41:56 +0400

    indexof iterator

--- iterators.js
+++ iterators.js
@@ -1,11 +1,9 @@
 //// Iterator Map ////
 const animals = ['Hen', 'elephant', 'llama', 'leopard', 
 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
-
 const secretMessage = animals.map(animal => {
       return animal[0];
 })
-
 console.log(secretMessage); // returns "HelloWorld";
 //// End Iterator Map
 
@@ -15,8 +13,6 @@ console.log(secretMessage); // returns "HelloWorld";
 
 //// Iterator forEach ////
 const fruits = ['mango', 'papaya', 'pineapple', 'apple'];
-
-// Iterate over fruits below
 fruits.forEach(fruit => {
   console.log(`I want to eat a ${fruit}`);
 });
@@ -26,21 +22,31 @@ fruits.forEach(fruit => {
 
 
 //// Iterator Filter ////
-
 const randomNumbers = [375, 200, 3.14, 7, 13, 852];
-
-// Call .filter() on randomNumbers below
 const smallNumbers = randomNumbers.filter(number => {
   return number < 250;
 }) // Returns numbers less than 250
 
 
 const favoriteWords = ['nostalgia', 'hyperbole', 'fervent', 'esoteric', 'serene'];
-
-
-// Call .filter() on favoriteWords below
 const longFavoriteWords = favoriteWords.filter(word => {
   return word.length > 7;
 }) // Returns words with length > 7
-
 //// End Iterator Filter ///
+
+
+
+
+
+
+
+//// IndexOf Iterator ////
+const animals = ['hippo', 'tiger', 'lion', 'seal', 'cheetah', 'monkey', 'salamander', 'elephant'];
+const foundAnimal = animals.findIndex(word => {
+  return word === 'elephant';
+}); // Finds index of word elephant
+
+const startsWithS = animals.findIndex(word => {
+    return word[0] === 's';
+  }) // Finds index of word starting with s
+/// End IndexOf Iterator ////
